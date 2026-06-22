1b1de2cb623a18581f540f00ce132b80e086641b
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-22 19:07:13 -0300

    adicionando calculo

---
+++ js/script_calculo.js
@@ -0,0 +1,26 @@
+export function calcularIPVA(valor, combustivel, idade){
+
+    if(idade > 20){
+        return "isento";
+    }
+
+    if (combustivel === "gasolina"){
+        return valor * 0.20
+    }
+
+    if (combustivel === "etanol"){
+        return valor * 0.15
+    }
+
+    if (combustivel === "bicombustivel"){
+        return valor * 0.10
+    }
+
+    if (combustivel === "hibrido"){
+        return valor * 0.08
+    }
+
+    if (combustivel === "eletrico"){
+        return valor * 0.02
+    }
+}
